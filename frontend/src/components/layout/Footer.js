d9bf30bd212ee64a34df78fff57249117ba1a099
rlaghdcjf12 <rlaghdcjf12@naver.com>
2022-06-19 01:18:16 +0900

    [FIX] 잔디 심기 테스트

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -3,10 +3,10 @@ const Footer = () => {
     <footer className='App-default_layout footer'>
       <nav className='container-md'>
         <div className='row'>
-          <div className='col-3'>메뉴 1</div>
-          <div className='col-3'>메뉴 2</div>
-          <div className='col-3'>메뉴 3</div>
-          <div className='col-3'>메뉴 4</div>
+          <div className='col-3'>M1</div>
+          <div className='col-3'>M2</div>
+          <div className='col-3'>M3</div>
+          <div className='col-3'>M4</div>
         </div>
       </nav>
     </footer>
