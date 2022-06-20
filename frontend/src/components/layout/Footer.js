a356c7482542b629b5e75e22381d88fcafae7715
rlaghdcjf12 <rlaghdcjf12@naver.com>
2022-06-20 20:45:22 +0900

    [UPDATE] css in js 적용
    - css in js 기술로 styled component 적용

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,15 +1,38 @@
+import styled from 'styled-components';
+
 const Footer = () => {
   return (
-    <footer className='App-default_layout footer'>
-      <nav className='container-md'>
+    <FooterEl>
+      <nav className='container'>
         <div className='row'>
-          <div className='col-3'>M1</div>
-          <div className='col-3'>M2</div>
-          <div className='col-3'>M3</div>
-          <div className='col-3'>M4</div>
+          <Btn className='col-3'>M1</Btn>
+          <Btn className='col-3'>M2</Btn>
+          <Btn className='col-3'>M3</Btn>
+          <Btn className='col-3'>M4</Btn>
         </div>
       </nav>
-    </footer>
+    </FooterEl>
   );
 };
+
+const FooterEl = styled.footer`
+  background-color: #282c34;
+  min-height: 6vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: calc(8px + 2vmin);
+
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
+
+const Btn = styled.div`
+  cursor: pointer;
+`;
+
 export default Footer;
--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,15 +1,33 @@
+import styled from 'styled-components';
+
 const Header = () => {
   return (
-    <header className='App-default_layout header'>
+    <HeaderEl>
       <div className='container'>
-        <div className='row'>
-          <div className='col-3'>PyeongBee</div>
-          <div className='col-2 offset-3'>M1</div>
-          <div className='col-2'>M2</div>
-          <div className='col-2'>M3</div>
-        </div>
+        <nav className='row'>
+          <Btn className='col-3'>PyeongBee</Btn>
+          <Btn className='col-2 offset-3'>M1</Btn>
+          <Btn className='col-2'>M2</Btn>
+          <Btn className='col-2'>M3</Btn>
+        </nav>
       </div>
-    </header>
+    </HeaderEl>
   );
 };
+
+const HeaderEl = styled.header`
+  background-color: #282c34;
+  min-height: 6vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: calc(10px + 2vmin);
+`;
+
+const Btn = styled.div`
+  cursor: pointer;
+`;
+
 export default Header;
