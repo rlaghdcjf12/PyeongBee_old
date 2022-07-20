2d4fef5878a3dc2e8eca29f39354262a6603326e
DESKTOP-FN4A2F7\rlagh <rlaghdcjf12@naver.com>
2022-07-20 23:48:48 +0900

    [UPDATE] 평비 ME - 자아 메뉴 상단 프로필 개발

--- frontend/src/components/common/Hexagon.js
+++ frontend/src/components/common/Hexagon.js
@@ -1,22 +1,43 @@
-import styled, { css } from 'styled-components';
-import Figure from 'react-bootstrap/Figure';
+import styled, { css } from "styled-components";
+import Figure from "react-bootstrap/Figure";
 
-const Hexagon = ({ type = 'horizontal', isRegular = false, image, width, height, unit = 'px', border = false }) => {
+const Hexagon = ({
+  type = "horizontal",
+  isRegular = false,
+  image,
+  width,
+  height,
+  unit = "px",
+  border = false,
+}) => {
   const newHeight = isRegular ? (width / 2) * 1.73 : height;
+
+  // TODO : image alt text 값 item에 따라서 바뀌도록 수정할 것.
 
   const hexaImage = (
     <Hexa width={width} height={newHeight} unit={unit}>
       {image ? (
-        <Image width={width} height={newHeight} unit={unit} src={image} alt='bee hive'></Image>
+        <Image
+          width={width}
+          height={newHeight}
+          unit={unit}
+          src={image}
+          alt="bee hive"
+        ></Image>
       ) : (
-        <Figure.Image width='100%' height='100%' src={image} alt='bee hive'></Figure.Image>
+        <Figure.Image
+          width="100%"
+          height="100%"
+          src={image}
+          alt="bee hive"
+        ></Figure.Image>
       )}
     </Hexa>
   );
 
   if (border) {
-    const borderSpec = border.split(' ');
-    const borderWeight = Number(borderSpec[0].replace('px', ''));
+    const borderSpec = border.split(" ");
+    const borderWeight = Number(borderSpec[0].replace("px", ""));
 
     return (
       <Border
@@ -43,9 +64,20 @@ const baseSize = css`
 `;
 const Border = styled.div`
   /* clip-path: polygon(50% 0, 100% 25%, 100% 75%, 50% 100%, 0 75%, 0 25%); // 세로 정육각형 */
-  clip-path: polygon(15% 0, 85% 0, 100% 50%, 85% 100%, 15% 100%, 0 50%); // 가로 정육각형
-  width: calc(${(props) => props.width}${(props) => props.unit} + ${(props) => props.borderWeight * 2}px);
-  height: calc(${(props) => props.height}${(props) => props.unit} + ${(props) => props.borderWeight * 2}px);
+  clip-path: polygon(
+    15% 0,
+    85% 0,
+    100% 50%,
+    85% 100%,
+    15% 100%,
+    0 50%
+  ); // 가로 정육각형
+  width: calc(
+    ${(props) => props.width}${(props) => props.unit} + ${(props) => props.borderWeight * 2}px
+  );
+  height: calc(
+    ${(props) => props.height}${(props) => props.unit} + ${(props) => props.borderWeight * 2}px
+  );
   max-width: calc(180px + ${(props) => props.borderWeight * 2}px);
   max-height: calc(180px + ${(props) => props.borderWeight * 2}px);
   padding: ${(props) => props.borderWeight}px;
@@ -53,7 +85,14 @@ const Border = styled.div`
 `;
 const Hexa = styled.div`
   /* clip-path: polygon(50% 0, 100% 25%, 100% 75%, 50% 100%, 0 75%, 0 25%); // 세로 정육각형 */
-  clip-path: polygon(15% 0, 85% 0, 100% 50%, 85% 100%, 15% 100%, 0 50%); // 가로 정육각형
+  clip-path: polygon(
+    15% 0,
+    85% 0,
+    100% 50%,
+    85% 100%,
+    15% 100%,
+    0 50%
+  ); // 가로 정육각형
   ${baseSize}
 `;
 const Image = styled.img`
--- frontend/src/pages/me/main.js
+++ frontend/src/pages/me/main.js
@@ -1,9 +1,76 @@
-import styled from 'styled-components';
+import { Alert } from "react-bootstrap";
+import Image from "react-bootstrap/esm/Image.js";
+import styled from "styled-components";
+import Hexagon from "../../components/common/Hexagon.js";
 
 const MeMain = () => {
-  return <div>ME 서비스의 메인 페이지 입니다.</div>;
+  return (
+    <>
+      <Profile>
+        <Background>
+          <Image
+            width={"100%"}
+            height={"100%"}
+            src="https://post-phinf.pstatic.net/MjAxOTA2MDNfMTQ3/MDAxNTU5NTI5MTkxNTI1.rxCH-JM27rrnvatW0sGIC9GnEArQzCIDXBA9QGyTSNYg.oGXpWK7prD3Jq9VyI1Ta9h7wZ2fUGIvMx8hC5q2_oZ8g.JPEG/Summer_beach.jpg?type=w1200"
+            alt="background_image"
+          ></Image>
+        </Background>
+        <HexagonContainer>
+          <Hexagon
+            image="https://st.depositphotos.com/2069237/2950/i/950/depositphotos_29500311-stock-photo-running-man-male-runner-in.jpg"
+            width={30}
+            height={30}
+            unit="vmin"
+            border="1px gold"
+          ></Hexagon>
+        </HexagonContainer>
+        <MyDreamContainer>
+          <MyDream variant="warning">평범함이 모여 비범함이 된다.</MyDream>
+        </MyDreamContainer>
+        <NameContainer>
+          <Title>근면성실한</Title>&nbsp;
+          <NickName>찌롱</NickName>
+        </NameContainer>
+      </Profile>
+      <BodyContainer>
+        <div>ME 서비스의 메인 페이지 입니다.</div>
+      </BodyContainer>
+    </>
+  );
 };
 
 // styles
+const Profile = styled.div``;
+const Background = styled.div`
+  width: 100vw;
+  max-width: 1200px;
+  height: 40vh;
+  max-height: 400px;
+  margin: 0 auto;
+`;
+const HexagonContainer = styled.div`
+  position: absolute;
+  top: calc(40vh - min(15vmin, 90px));
+  left: calc(50vw - min(15vmin, 90px));
+`;
+const MyDreamContainer = styled.div`
+  position: absolute;
+  top: calc(calc(40vh + min(15vmin, 90px)) - 3.5vh);
+  left: 25vw;
+`;
+const MyDream = styled(Alert)`
+  width: 50vw;
+  height: 6vh;
+`;
+const NameContainer = styled.div`
+  margin-top: 15vh;
+  display: inline-flex;
+  font-size: calc(10px + 1.5vmin);
+`;
+const Title = styled.div``;
+const NickName = styled.div``;
+const BodyContainer = styled.div`
+  margin-top: 2vh;
+`;
 
 export default MeMain;
