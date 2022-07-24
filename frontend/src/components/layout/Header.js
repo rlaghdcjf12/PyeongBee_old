1cb15e7e866ed7c1124e19cdef3a6574f6bd9849
DESKTOP-FN4A2F7\rlagh <rlaghdcjf12@naver.com>
2022-07-24 21:14:42 +0900

    [UPDATE] 헤더 컴포넌트 -> 부트스트랩 Navbar로 변경

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,67 +1,103 @@
+import { useState, useEffect, useRef } from 'react';
+import { Container, Image, Navbar, Nav } from 'react-bootstrap';
+import { useWindowScroll } from 'react-use';
+import { throttle } from 'lodash';
 import styled from 'styled-components';
 
 const Header = ({ currentService, serviceList, changeService }) => {
+  const scrollInfo = useWindowScroll();
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [headerHide, setHeaderHide] = useState(false);
+
+  const handleScroll = (lastY, scroll) => {
+    if (lastY < scroll.y && scroll.y > 52) {
+      setHeaderHide(true);
+    } else if (lastY > scroll.y) {
+      setHeaderHide(false);
+    }
+    setLastScrollY(scroll.y);
+  };
+
+  const throttled = useRef(
+    throttle((lastY, scroll) => handleScroll(lastY, scroll), 100)
+  );
+
+  useEffect(() => {
+    throttled.current(lastScrollY, scrollInfo);
+  }, [scrollInfo, lastScrollY]);
+
   return (
-    <HeaderEl>
-      <div className='container'>
-        <FirstHeader className='row'>
-          <div className='col-3'>PyeongBee</div>
-          <div className='col-1 offset-6'>1</div>
-          <div className='col-1'>2</div>
-          <div className='col-1'>3</div>
-        </FirstHeader>
-        <SecondHeader className='row'>
-          <div className='col-2' onClick={() => changeService(serviceList[0])}>
-            {currentService?.id === 1 ? (
-              <Active>{serviceList[0].title}</Active>
-            ) : (
-              <Btn>{serviceList[0].title}</Btn>
-            )}
-          </div>
-          {/* <div className='col-2' onClick={() => changeService(serviceList[1])}>
-            {currentService?.id === 2 ? <Active>{serviceList[1].title}</Active> : <Btn>{serviceList[1].title}</Btn>}
-          </div> */}
-          <div className='col-2' onClick={() => changeService(serviceList[2])}>
-            {currentService?.id === 3 ? (
-              <Active>{serviceList[2].title}</Active>
-            ) : (
-              <Btn>{serviceList[2].title}</Btn>
-            )}
-          </div>
-        </SecondHeader>
-      </div>
-    </HeaderEl>
+    <>
+      <HeaderContainer collapseOnSelect expand='sm' headerHide={headerHide}>
+        <Container>
+          <Logo onClick={() => changeService(serviceList[0])}>
+            <Image
+              alt='logo'
+              src='/images/logo_Bee_lsh_white.png'
+              width='40'
+              height='40'
+            />
+            {' PyeongBee'}
+          </Logo>
+          <Navbar.Toggle aria-controls='navbar' />
+          <Navbar.Collapse id='navbar'>
+            <Nav
+              className='me-auto my-2 my-lg-0'
+              style={{ maxHeight: '100px' }}
+              navbar
+            >
+              <Service
+                active={currentService?.id === 1}
+                onClick={() => changeService(serviceList[0])}
+              >
+                Me
+              </Service>
+              {/* <Service
+                bold={currentService?.id === 2}
+                onClick={() => changeService(serviceList[1])}
+              >
+                You
+              </Service> */}
+              <Service
+                active={currentService?.id === 3}
+                onClick={() => changeService(serviceList[2])}
+              >
+                Us
+              </Service>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </HeaderContainer>
+      <EmptyArea />
+    </>
   );
 };
 
 // styles
-const HeaderEl = styled.header`
-  position: relative;
+const HeaderContainer = styled(Navbar)`
+  position: fixed;
+  display: ${(props) => (props.headerHide === true ? 'none' : 'block')};
+  z-index: 10;
+  width: 100vw;
+  padding: 0;
   background-color: white;
-  border-bottom: solid 1px gold;
-  min-height: 6vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: saddlebrown;
-  font-size: calc(7px + 2vmin);
+  border-bottom: solid 2px gold;
 `;
-const FirstHeader = styled.div`
-  /* position: fixed; */
-`;
-const SecondHeader = styled.nav`
-  /* position: fixed; */
-`;
-const Active = styled.span`
-  font-size: calc(6px + 2vmin);
-  font-weight: 600;
+const Logo = styled(Navbar.Brand)`
+  font-size: 26px;
+  line-height: 40px;
   cursor: pointer;
 `;
-const Btn = styled.span`
-  color: lightgray;
-  font-size: calc(6px + 2vmin);
+const Service = styled(Nav.Item)`
+  margin: 0 10px;
+  font-weight: ${(props) => (props.active ? '600' : '0')};
+  font-size: 22px;
+  color: ${(props) => (props.active ? 'black' : 'gray')};
   cursor: pointer;
+`;
+const EmptyArea = styled.div`
+  display: ${(props) => (props.headerHide === true ? 'none' : 'block')};
+  height: 52px;
 `;
 
 export default Header;
