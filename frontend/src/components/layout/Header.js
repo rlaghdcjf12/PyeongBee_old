66ed41bfa7b9698e5d3c078f605c66f66b570f94
DESKTOP-FN4A2F7\rlagh <rlaghdcjf12@naver.com>
2022-09-13 23:57:40 +0900

    [FIX] UI 컴포넌트 react-bootstrap -> mui 변경 작업 시작

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,107 +1,155 @@
 import { useState, useEffect, useRef } from 'react';
-import { Container, Image, Navbar, Nav } from 'react-bootstrap';
-import { useWindowScroll } from 'react-use';
-import { throttle } from 'lodash';
+// import { Container, Image, Navbar, Nav } from 'react-bootstrap';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 import styled from 'styled-components';
+import {
+  Slide,
+  AppBar,
+  CssBaseline,
+  Toolbar,
+  Typography,
+  Container,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Button,
+} from '@mui/material';
+import { MdMenu } from 'react-icons/md';
+import FlexBox from 'components/common/FlexBox';
 
-const Header = ({ currentService, serviceList, changeService }) => {
-  const scrollInfo = useWindowScroll();
-  const [lastScrollY, setLastScrollY] = useState(0);
-  const [headerHide, setHeaderHide] = useState(false);
+const Header = (props) => {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
-  const handleScroll = (lastY, scroll) => {
-    if (lastY < scroll.y && scroll.y > 52) {
-      setHeaderHide(true);
-    } else if (lastY > scroll.y) {
-      setHeaderHide(false);
-    }
-    setLastScrollY(scroll.y);
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
   };
 
-  const throttled = useRef(
-    throttle((lastY, scroll) => handleScroll(lastY, scroll), 100)
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const HideOnScroll = ({ children, window }) => {
+    const trigger = useScrollTrigger({
+      target: window ? window() : undefined,
+    });
+
+    return (
+      <Slide appear={false} direction='down' in={!trigger}>
+        {children}
+      </Slide>
+    );
+  };
+
+  const SmallAppBar = () => (
+    <Toolbar variant='dense' disableGutters sx={{ display: { xs: 'flex', md: 'none' } }}>
+      <ImageWrapper>
+        <img alt='logo' src='/images/logo_Bee_lsh_white.png' width='40' height='40' />
+      </ImageWrapper>
+      <Typography variant='h6' component='div'>
+        PyeongBee
+      </Typography>
+      <Box>
+        <IconButton
+          size='middle'
+          aria-label='account of current user'
+          aria-controls='menu-appbar'
+          aria-haspopup='true'
+          onClick={handleOpenNavMenu}
+          color='inherit'
+        >
+          <MdMenu />
+        </IconButton>
+        <Menu
+          id='menu-appbar'
+          anchorEl={anchorElNav}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorElNav)}
+          onClose={handleCloseNavMenu}
+          sx={{
+            display: { xs: 'block', md: 'none' },
+          }}
+        >
+          <MenuItem key={1} onClick={handleCloseNavMenu}>
+            <Typography textAlign='center'>Me</Typography>
+          </MenuItem>
+          <MenuItem key={1} onClick={handleCloseNavMenu}>
+            <Typography textAlign='center'>Us</Typography>
+          </MenuItem>
+        </Menu>
+      </Box>
+    </Toolbar>
   );
 
-  useEffect(() => {
-    throttled.current(lastScrollY, scrollInfo);
-  }, [scrollInfo, lastScrollY]);
+  const NormalAppBar = () => (
+    <Toolbar variant='dense' disableGutters sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+      <ImageWrapper>
+        <img alt='logo' src='/images/logo_Bee_lsh_white.png' width='40' height='40' />
+      </ImageWrapper>
+      <Typography variant='h6' component='div'>
+        PyeongBee
+      </Typography>
+      <Box>
+        <Button key={1} onClick={handleCloseNavMenu}>
+          <Typography textAlign='center'>Me</Typography>
+        </Button>
+        <Button key={2} onClick={handleCloseNavMenu}>
+          <Typography textAlign='center'>Us</Typography>
+        </Button>
+      </Box>
+    </Toolbar>
+  );
 
   return (
     <>
-      <HeaderContainer
-        collapseOnSelect
-        expand='sm'
-        headerhide={headerHide ? 'true' : 'false'}
-      >
-        <Container>
-          <Logo onClick={() => changeService(serviceList[0])}>
-            <Image
-              alt='logo'
-              src='/images/logo_Bee_lsh_white.png'
-              width='40'
-              height='40'
-            />
-            {' PyeongBee'}
-          </Logo>
-          <Navbar.Toggle aria-controls='navbar' />
-          <Navbar.Collapse id='navbar'>
-            <Nav
-              className='me-auto my-2 my-lg-0'
-              style={{ maxHeight: '100px' }}
-              navbar
-            >
-              <Service
-                active={currentService?.id === 1 ? 'true' : 'false'}
-                onClick={() => changeService(serviceList[0])}
-              >
-                Me
-              </Service>
-              {/* <Service
-                bold={currentService?.id === 2}
-                onClick={() => changeService(serviceList[1])}
-              >
-                You
-              </Service> */}
-              <Service
-                active={currentService?.id === 3 ? 'true' : 'false'}
-                onClick={() => changeService(serviceList[2])}
-              >
-                Us
-              </Service>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </HeaderContainer>
-      <EmptyArea />
+      <HideOnScroll {...props}>
+        <AppBar elevation={0}>
+          <HeaderContainer maxWidth='xl'>
+            <SmallAppBar />
+            <NormalAppBar />
+          </HeaderContainer>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
     </>
   );
 };
 
 // styles
-const HeaderContainer = styled(Navbar)`
-  position: fixed;
-  display: ${(props) => (props.headerhide === 'true' ? 'none' : 'block')};
-  z-index: 10;
-  width: 100vw;
-  padding: 0;
+const HeaderContainer = styled(Container)`
   background-color: white;
   border-bottom: solid 2px gold;
+  color: black;
 `;
-const Logo = styled(Navbar.Brand)`
-  font-size: 26px;
-  line-height: 40px;
-  cursor: pointer;
+const ImageWrapper = styled.div`
+  margin-right: 10px;
 `;
-const Service = styled(Nav.Item)`
-  margin: 0 10px;
-  font-weight: ${(props) => (props.active === 'true' ? '600' : '0')};
-  font-size: 22px;
-  color: ${(props) => (props.active === 'true' ? 'black' : 'gray')};
-  cursor: pointer;
-`;
-const EmptyArea = styled.div`
-  display: ${(props) => (props.headerHide === 'true' ? 'none' : 'block')};
-  height: 52px;
-`;
+// const Service = styled(Nav.Item)`
+//   margin: 0 10px;
+//   font-weight: ${(props) => (props.active === 'true' ? '600' : '0')};
+//   font-size: 22px;
+//   color: ${(props) => (props.active === 'true' ? 'black' : 'gray')};
+//   cursor: pointer;
+// `;
+// const EmptyArea = styled.div`
+//   display: ${(props) => (props.headerHide === 'true' ? 'none' : 'block')};
+//   height: 52px;
+// `;
 
 export default Header;
